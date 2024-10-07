bcd207dc460c584a10c681244abd433d8b353ab0
Hyangrim Lee <hyangrim.lee@kims.co.kr>
2024-10-07 16:51:39 +0900

    기본 구조 잡기 - 1

--- fanletter/letters/urls.py
+++ fanletter/letters/urls.py
@@ -4,5 +4,6 @@ from . import views
 
 app_name = "letters"
 urlpatterns = [
-    path("", views.letter_list, name="letter_list")
+    path("", views.letter_list, name="letter_list"),
+    path("<int:pk>/", views.letter_view, name="letter_view"),
 ]
--- fanletter/letters/views.py
+++ fanletter/letters/views.py
@@ -1,6 +1,16 @@
 from django.shortcuts import render
-from .models import Letter
+from .models import Letter, Song
 
 def letter_list(request):
-    objects = Letter.objects.all()
-    return render(request,"letters/index.html" , {'objects': objects})
+    letters = Letter.objects.filter(is_open=True).select_related("song")
+
+    return render(request,"letters/list.html" , {'letters': letters})
+
+def letter_view(request, pk):
+    if pk == 5:
+        return letter_5(request)
+    else:
+        return render(request, "letters/letter.html")
+
+def letter_5(request):
+    return render(request, "letters/unlucky.html")
