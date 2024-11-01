f99de4f3c672ea557bd49cbe4f177cb733dae4f3
Hyangrim Lee <hyangrim.lee@kims.co.kr>
2024-11-01 19:58:39 +0900

    unlucky main base

--- fanletter/letters/urls.py
+++ fanletter/letters/urls.py
@@ -6,4 +6,6 @@ app_name = "letters"
 urlpatterns = [
     path("", views.letter_list, name="letter_list"),
     path("<int:pk>/", views.letter_view, name="letter_view"),
+    path("unlucky/", views.unlucky_list, name="unlucky_list"),
+    path("unlucky/<int:pk>/", views.unlucky_view, name="unlucky_view"),
 ]
